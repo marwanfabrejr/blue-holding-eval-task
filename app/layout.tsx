434d53fa802ec7bd1fb115e84c4c048784c1f0cf
Marwan Zayed <marwanzayed4444@gmail.com>
2023-10-25 13:02:10 +0300

    Layout Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body className={`${inter.className} overflow-scroll`}>
+      <body className={inter.className}>
         <Providers>
           <ToasterContext />
           <Navbar />
